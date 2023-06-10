5ef635613cf0d9bddabec5328a00b2aeafb7e93a
Ardikaas <ardikaajisetiawan@gmail.com>
2023-06-10 14:00:28 +0700

    make simple navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './component/button';
+import Navbar from './component/Navbar';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           test 123 
